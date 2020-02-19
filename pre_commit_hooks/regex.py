b0f9bc9598e9d6f1392d3b723748851c88bad47d
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-02-19 17:25:30 +0100

    Fix code styling

--- pre_commit_hooks/regex.py
+++ pre_commit_hooks/regex.py
@@ -7,43 +7,42 @@ import os
 import sys
 
 if sys.platform.lower() == "win32":
-    os.system('color')
+    os.system("color")
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    parser.add_argument("filenames", nargs="*", help="Filenames to check")
+    parser.add_argument("--pattern", help="Pattern to check")
+    parser.add_argument("--custom_message", help="Custom message to print")
     parser.add_argument(
-        '--pattern',
-        help='Pattern to check'
-    )
-    parser.add_argument('--custom_message', help='Custom message to print')
-    parser.add_argument(
-        '--critical',
-        help='If active the regex matching should block the commit',
+        "--critical", help="If active the regex matching should block the commit",
     )
     args = parser.parse_args(argv)
-    print(args)
 
     files_containing = []
     is_critical = args.critical is not None
 
     for filename in args.filenames:
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             line = f.read()
-            files_containing.append(filename) if re.search(re.compile(args.pattern.encode()), line) else None
+            files_containing.append(filename) if re.search(
+                re.compile(args.pattern.encode()), line
+            ) else None
 
     if files_containing:
-        color = '\033[31m' if is_critical else '\033[33m'
-        icon = emoji.emojize(':no_entry:') if is_critical else emoji.emojize(':bulb:', use_aliases=True)
+        color = "\033[31m" if is_critical else "\033[33m"
+        icon = (
+            emoji.emojize(":no_entry:")
+            if is_critical
+            else emoji.emojize(":bulb:", use_aliases=True)
+        )
         for file_contaning in files_containing:
-            print(
-                f'{color}{icon} {file_contaning}: {args.custom_message}\033[0m'
-            )
-        if (is_critical):
+            print(f"{color}{icon} {file_contaning}: {args.custom_message}\033[0m")
+        if is_critical:
             return 1
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
