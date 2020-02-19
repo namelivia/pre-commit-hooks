bfd13329040e88ac9ec404e8314e0e4e039a32ee
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-02-19 12:19:14 +0100

    Add a expression list

--- pre_commit_hooks/regex.py
+++ pre_commit_hooks/regex.py
@@ -2,21 +2,11 @@ import argparse
 from typing import Optional
 from typing import Sequence
 
-BLACKLIST = [
-    b'BEGIN RSA PRIVATE KEY',
-    b'BEGIN DSA PRIVATE KEY',
-    b'BEGIN EC PRIVATE KEY',
-    b'BEGIN OPENSSH PRIVATE KEY',
-    b'BEGIN PRIVATE KEY',
-    b'PuTTY-User-Key-File-2',
-    b'BEGIN SSH2 ENCRYPTED PRIVATE KEY',
-    b'BEGIN PGP PRIVATE KEY BLOCK',
-]
-
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    parser.add_argument('expressions', nargs='*', help='Expressions to check')
     parser.add_argument('--custom_message', help='Custom message to print')
     parser.add_argument(
         '--critical',
@@ -24,24 +14,20 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         default=False
     )
     args = parser.parse_args(argv)
-    print('Testing for custom message')
-    print(args.custom_message)
-    if (args.critical):
-        return 1
-    return 0
 
-    private_key_files = []
+    files_containing = []
 
     for filename in args.filenames:
         with open(filename, 'rb') as f:
             content = f.read()
-            if any(line in content for line in BLACKLIST):
-                private_key_files.append(filename)
+            if any(line in content for line in args.expressions):
+                files_containing.append(filename)
 
-    if private_key_files:
-        for private_key_file in private_key_files:
-            print(f'Private key found: {private_key_file}')
-        return 1
+    if files_containing:
+        for file_contaning in files_containing:
+            print(f'{file_contaning}: {args.custom_message}')
+        if (args.critical):
+            return 1
     else:
         return 0
 
