d49e12994e2e21b62d2e5abf2b0d6f05a08821fb
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-02-20 09:52:28 +0100

    Remove critical flag value requirement

--- pre_commit_hooks/regex.py
+++ pre_commit_hooks/regex.py
@@ -16,12 +16,13 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument("--pattern", help="Pattern to check")
     parser.add_argument("--custom_message", help="Custom message to print")
     parser.add_argument(
-        "--critical", help="If active the regex matching should block the commit",
+        "--critical",
+        help="If present the regex matching should block the commit",
+        action="store_true",
     )
     args = parser.parse_args(argv)
 
     files_containing = []
-    is_critical = args.critical is not None
 
     for filename in args.filenames:
         with open(filename, "rb") as f:
@@ -31,15 +32,15 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             ) else None
 
     if files_containing:
-        color = "\033[31m" if is_critical else "\033[33m"
+        color = "\033[31m" if args.critical else "\033[33m"
         icon = (
             emoji.emojize(":no_entry:")
-            if is_critical
+            if args.critical
             else emoji.emojize(":bulb:", use_aliases=True)
         )
         for file_contaning in files_containing:
             print(f"{color}{icon} {file_contaning}: {args.custom_message}\033[0m")
-        if is_critical:
+        if args.critical:
             return 1
     return 0
 
